8f6630945c2b418e7efaa9d0aeb8544827124b19
AzzamBilalMukarrom <bilaldenker020@gmail.com>
2025-05-08 16:02:00 +0700

    buat bool listempty

--- LinkList.cpp
+++ LinkList.cpp
@@ -26,12 +26,31 @@ public:
         Node *Nodebaru = new Node;
         Nodebaru->noMhs = nim;
 
-        if (start != NULL || nim <= start->noMhs)
+        if (start != NULL || nim <= start->noMhs){
             if ((start != NULL) && (nim == start->noMhs)){
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
             }
-            
+            Nodebaru->next = start;
+            start = Nodebaru;
+            return;
+        }
+        Node *previous = start;
+    Node *current = start;
 
+    while ((current != NULL) && (nim >= current->noMhs)){
+        if (nim == current->noMhs){
+            cout << "\nDuplikasi noMhs tidak diijinkan|n";
+            return;
+        }
+        previous = current;
+        current = current->next;
     }
-}
+    Nodebaru->next = current;
+    previous->next = Nodebaru;
+    }
+
+    bool listempty(){
+        return (start == NULL);
+    }
+
